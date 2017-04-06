fb9e78bb9eeabbd88eee9ef944efffff1b6157a6
Richard Saunders <richard@sdevsolutions.com>
2017-04-06 08:12:18 +0100

    more unit tests

--- spec/promise-pool-spec.js
+++ spec/promise-pool-spec.js
@@ -1,9 +1,9 @@
 var PromisePool = require('../promise-pool');
 
 describe("PromisePool", function() {
-    describe("get", function() {
+    describe("call to get", function() {
 
-        it('returns the data from the set promise resolver', function(done) {
+        it('returns a resolved promise with data from the resolver', function(done) {
             var expectedData = 'data returned from promise';
             var promisePool = new PromisePool();
             promisePool.set('promise_key_here', function(resolve) {
@@ -12,6 +12,22 @@ describe("PromisePool", function() {
             promisePool.get('promise_key_here')
                 .then(function(actualData) {
                     expect(actualData).toBe(expectedData);
+                    done();
+                });
+        });
+
+        it('returns a rejected promise with error from the resolver', function(done) {
+            var expectedError = 'error returned from promise';
+            var promisePool = new PromisePool();
+            promisePool.set('promise_key_here', function(resolve, reject) {
+                reject(expectedError);
+            });
+            promisePool.get('promise_key_here')
+                .then(function () {
+                    throw new Error('promise did not reject');
+                })
+                .catch(function(actualError) {
+                    expect(actualError).toBe(expectedError);
                     done();
                 });
         });
@@ -48,7 +64,7 @@ describe("PromisePool", function() {
                 });
         });
 
-        it('uses the set resolver', function(done) {
+        it('calls the resolver', function(done) {
             var promisePool = new PromisePool();
             var observer = {
                 resolver: function(resolve) {
@@ -63,5 +79,139 @@ describe("PromisePool", function() {
                     done();
                 });
         });
+    });
+
+    describe('multiple calls to get while promise still pending', function () {
+
+        it('returns same resolved promise with data from the resolver', function(done) {
+            var expectedData = 'data returned from promise';
+            var promisePool = new PromisePool();
+            var observer = {
+                resolver: function(resolve) {
+                    setTimeout(function() { resolve(expectedData) }, 100);
+                }
+            };
+            var resolverSpy = spyOn(observer, 'resolver').and.callThrough();
+
+            jasmine.clock().install();
+            promisePool.set('promise_key_here', resolverSpy);
+            promisePool.get('promise_key_here')
+                .then(function(actualData) {
+                    expect(actualData).toBe(expectedData);
+                });
+
+            promisePool.get('promise_key_here')
+                .then(function(actualData) {
+                    expect(actualData).toBe(expectedData);
+                });
+
+            promisePool.get('promise_key_here')
+                .then(function(actualData) {
+                    expect(actualData).toBe(expectedData);
+                    done();
+                });
+
+            jasmine.clock().tick(101);
+            jasmine.clock().uninstall();
+        });
+
+        it('only calls the resolver once', function(done) {
+            var expectedData = 'data returned from promise';
+            var promisePool = new PromisePool();
+            var observer = {
+                resolver: function(resolve) {
+                    setTimeout(function() { resolve(expectedData) }, 100);
+                }
+            };
+            var resolverSpy = spyOn(observer, 'resolver').and.callThrough();
+
+            jasmine.clock().install();
+            promisePool.set('promise_key_here', resolverSpy);
+            promisePool.get('promise_key_here');
+            promisePool.get('promise_key_here');
+
+            promisePool.get('promise_key_here')
+                .then(function() {
+                    expect(resolverSpy).toHaveBeenCalledTimes(1);
+                    done();
+                });
+
+            jasmine.clock().tick(101);
+            jasmine.clock().uninstall();
+        });
+
+        it('returns same rejected promise with error from the resolver', function(done) {
+            var expectedError = 'error returned from promise';
+            var promisePool = new PromisePool();
+
+            jasmine.clock().install();
+            promisePool.set('promise_key_here', function(resolve, reject) {
+                setTimeout(function() { reject(expectedError); }, 100);
+            });
+
+            promisePool.get('promise_key_here')
+                .then(function () {
+                    throw new Error('promise did not reject');
+                })
+                .catch(function(actualError) {
+                    expect(actualError).toBe(expectedError);
+                });
+
+            promisePool.get('promise_key_here')
+                .then(function () {
+                    throw new Error('promise did not reject');
+                })
+                .catch(function(actualError) {
+                    expect(actualError).toBe(expectedError);
+                });
+
+            promisePool.get('promise_key_here')
+                .then(function () {
+                    throw new Error('promise did not reject');
+                })
+                .catch(function(actualError) {
+                    expect(actualError).toBe(expectedError);
+                    done();
+                });
+
+            jasmine.clock().tick(101);
+            jasmine.clock().uninstall();
+        });
+    });
+
+    describe('multiple calls to get after promise resolved', function () {
+
+        it('returns new resolved promises with data from the resolver', function(done) {
+            var increment = 0;
+            var promisePool = new PromisePool();
+            var observer = {
+                resolver: function(resolve) {
+                    resolve(++increment);
+                }
+            };
+            var resolverSpy = spyOn(observer, 'resolver').and.callThrough();
+
+            promisePool.set('promise_key_here', resolverSpy);
+            promisePool.get('promise_key_here')
+                .then(function(actualData) {
+
+                    expect(actualData).toBe(1);
+                    expect(resolverSpy).toHaveBeenCalledTimes(1);
+
+                    promisePool.get('promise_key_here')
+                        .then(function(actualData) {
+
+                            expect(actualData).toBe(2);
+                            expect(resolverSpy).toHaveBeenCalledTimes(2);
+
+                            promisePool.get('promise_key_here')
+                                .then(function (actualData) {
+                                    expect(actualData).toBe(3);
+                                    expect(resolverSpy).toHaveBeenCalledTimes(3);
+                                    done();
+                                });
+                        });
+                });
+            });
     });
 });
